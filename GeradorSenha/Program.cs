d9590aee3ab0d2c1fd6ab860f41c3ce8b30da5e0
fernanda stocler <nandasastocler@gmail.com>
2025-01-20 14:06:27 -0300

    adicionado atividade GeradorSenhas

---
+++ GeradorSenha/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("GeradorSenhas!");
+
+double tamanho =
+       inclusão de símbolos =
+       
+       Console.WriteLine("Digite o tamanho desejado: ");
+       if(double.TryParse(Console.ReadLine(), out double GeradorSenhas))
+       {
+        Console.WriteLine("Digite a inclusão de símbolos desejados: ");
+        string GeradorSenhas = Console.ReadLine();
+        switch ()
+               }
